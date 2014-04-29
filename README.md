cba52bd07947b7c13dc81c4ca996193832b01800
Greg Little <glittle@gmail.com>
2014-04-29 18:46:34 -0500

    closer..

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ buddhapong
 to set up:
 
 	cordova plugin add https://github.com/songz/cordova-plugin-opentok/
+	cordova plugin add https://github.com/mkuklis/phonegap-websocket
 	cordova platform add android
 	cordova platform add ios
 
--- www/code.js
+++ www/code.js
@@ -1,4 +1,4 @@
-try {
+
 var waitFor = {}
 var isApp = typeof cordova != 'undefined'
 var buddhapongServer = 'http://buddhapong-server-env-w434ankmsv.elasticbeanstalk.com'
@@ -46,40 +46,42 @@ function main() {
     ws.onclose = function (e) {
         $('#main').empty().text('SOCKET CLOSED')
     }
+
     ws.send(_.json('hello, I have arrived: ' + Math.random()))
+    $.post(buddhapongServer + '/createSession', null, function (session) {
+        ws.send(_.json({ session : session }))
+    })
+
 }
 main()
-} catch (e) {
-    $(function () {
-        $('body').append($('<div/>').text('err: ' + e))
-    })
-}
 
-// document.addEventListener('deviceready', function() {
-//       // Getting OpenTokRTC's room's credentials. 
-//       // To use your own room in opentokrtc, change cordova to room of your choice
-//       //   -> ie: https://opentokrtc.com/myroom.json
-//       // To use your own credentials
-//       //  replace data.apiKey, data.sid, and data.token with your own 
-//       var xmlhttp=new XMLHttpRequest();
-//       xmlhttp.open("GET", "https://opentokrtc.com/cordova.json", false);
-//       xmlhttp.send();
-//       var data = JSON.parse( xmlhttp.response );
+/*
+document.addEventListener('deviceready', function() {
+      // Getting OpenTokRTC's room's credentials. 
+      // To use your own room in opentokrtc, change cordova to room of your choice
+      //   -> ie: https://opentokrtc.com/myroom.json
+      // To use your own credentials
+      //  replace data.apiKey, data.sid, and data.token with your own 
+      var xmlhttp=new XMLHttpRequest();
+      xmlhttp.open("GET", "https://opentokrtc.com/cordova.json", false);
+      xmlhttp.send();
+      var data = JSON.parse( xmlhttp.response );
 
-//       // Very simple OpenTok Code for group video chat
-//       var publisher = TB.initPublisher(data.apiKey,'myPublisherDiv');
+      // Very simple OpenTok Code for group video chat
+      var publisher = TB.initPublisher(data.apiKey,'myPublisherDiv');
 
-//       var session = TB.initSession( data.apiKey, data.sid ); 
-//       session.on({
-//         'streamCreated': function( event ){
-//             var div = document.createElement('div');
-//             div.setAttribute('id', 'stream' + event.stream.streamId);
-//             document.body.appendChild(div);
-//             session.subscribe( event.stream, div.id, {subscribeToAudio: false} );
-//         }
-//       });
-//       session.connect(data.token, function(){
-//         session.publish( publisher );
-//       });
+      var session = TB.initSession( data.apiKey, data.sid ); 
+      session.on({
+        'streamCreated': function( event ){
+            var div = document.createElement('div');
+            div.setAttribute('id', 'stream' + event.stream.streamId);
+            document.body.appendChild(div);
+            session.subscribe( event.stream, div.id, {subscribeToAudio: false} );
+        }
+      });
+      session.connect(data.token, function(){
+        session.publish( publisher );
+      });
 
-// }, false);
+}, false);
+*/
